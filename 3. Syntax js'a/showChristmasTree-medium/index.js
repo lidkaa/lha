6e776f0429e7aa144401b211653ef54ed834cb07
lidkaa <lidka@cuvs.net>
2023-07-04 14:21:00 +0200

    after first code review

--- 3. Syntax js'a/showChristmasTree-medium/index.js
+++ 3. Syntax js'a/showChristmasTree-medium/index.js
@@ -1,20 +1,41 @@
+import { validation } from "../../validation.js";
 
-function showChristmasTree(treeHeight) {
-    if (!treeHeight || typeof(treeHeight) != 'number') {return}
-    if (treeHeight <=0 || treeHeight > 10) {return}
+function showChristmasTree(treeHeight = 5) {
+    validation({
+        value: treeHeight,
+        type: 'Number',
+    })
+
+    if (treeHeight <= 0 || treeHeight > 10) throw new Error('Pass values between 1 and 10');
+
+    const leaf = 'x'
+    const levels = leaf.repeat(treeHeight);
+    const emptySpace = ' '
+    const leftSideOfTree = '/'
+    const rightSideOfTree = '\\'
+    const topOfTree = emptySpace.repeat(treeHeight) + '*';
+    const bottomOfTree = emptySpace.repeat(treeHeight) + '|';
+
     let i = 0;
     let j = treeHeight;
-    let str = 'x'
-    let rows = 'x'.repeat(treeHeight);
+    let result = '';
 
-    for (let element of rows) {
-        if (i === 0) {
-            console.log((' ').repeat(treeHeight) + '*');
-        }
+    console.log(topOfTree);
+
+    for (const _ of levels) {
+
         i = i + 2;
         j = j - 1;
-        console.log((' ').repeat(j) + '/' + str.repeat(i - 1) + '\\');  
+
+        const repeatedEmptySpaces = emptySpace.repeat(j)
+        const repeatedContent = leaf.repeat(i - 1)
+        const leavesLevel = `${repeatedEmptySpaces}${leftSideOfTree}${repeatedContent}${rightSideOfTree}`
+        console.log(leavesLevel);
     }
+
+    console.log(bottomOfTree);
+
+    return result
 }
 
-showChristmasTree(4);
+console.log(showChristmasTree(5));
