5ac9d19cde51ee6b7959fbea9e5d87e391f16720
lidkaa <lidka@cuvs.net>
2023-07-05 15:19:34 +0200

    tasks 3rd and  4th module

--- 4. Funkcje w praktyce/isValidPhoneNumber-medium/index.js
+++ 4. Funkcje w praktyce/isValidPhoneNumber-medium/index.js
@@ -1,3 +1,31 @@
+import { validation } from "../../validation.js";
+
 function isValidPhoneNumber(value) {
-	// kod
+	const errorMessage = 'Pass valid phone number: "123456789", "123-123-123", "123 123 123"';
+	const valueType = typeof (value);
+	let checked = false
+	let properValue = typeof (value) === 'number' ? value.toString() : value;
+
+	if (valueType != 'string' && valueType != 'number') throw new Error(errorMessage);
+
+	validation({
+		value: properValue,
+		type: 'String'
+	});
+
+	let phoneNumberPatterns = [
+		/^\d{9}$/,
+		/^\d\d\d-\d\d\d-\d\d\d$/,
+		/^\d\d\d \d\d\d \d\d\d$/
+	];
+
+	for (const pattern of phoneNumberPatterns) {
+		if (pattern.test(properValue)) checked = true;
+	}
+
+	if (checked === false) throw new Error(errorMessage);
+
+	return checked;
 }
+
+console.log(isValidPhoneNumber(123456789));
