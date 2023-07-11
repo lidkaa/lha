3374ac66a4da9a3198394b20dbd4e8a20843d576
lidkaa <lidka@cuvs.net>
2023-07-11 15:31:35 +0200

    4th module task areTheyEqual

--- 4. Funkcje w praktyce/areTheyEqual-hard/index.js
+++ 4. Funkcje w praktyce/areTheyEqual-hard/index.js
@@ -1,3 +1,50 @@
 function areTheyEqual(firstValue, secondValue) {
-	// kod
+
+	if (!firstValue || !secondValue) throw new Error('pass a proper value')
+
+	const isTypeObject = typeof (firstValue, secondValue) === 'object' ? true : false;
+	const isArray = firstValue instanceof Array && secondValue instanceof Array ? true : false;
+	const isArrayLengthEqual = firstValue.length === secondValue.length ? true : false;
+
+	//objects
+	if (isTypeObject && !isArray) {
+		const firstValueKeys = Object.keys(firstValue).sort();
+		const secondValueKeys = Object.keys(secondValue).sort();
+		const areKeysLengthEqual = firstValueKeys.length === secondValueKeys.length ? true : false;
+
+
+		if (areKeysLengthEqual) {
+			return firstValueKeys.every(element => {
+				console.log(element, firstValue[element], secondValue[element]);
+				let equality = firstValue[element] === secondValue[element];
+				return equality
+			})
+		}
+	}
+
+	// //arrays
+	if (isArray && isArrayLengthEqual) {
+		return firstValue.every((element, index) => element === secondValue[index]);
+	}
+
+	//primitives vs ...
+	return firstValue === secondValue ? true : false;
 }
+
+
+// let firstValue = {
+// 	a: 1,
+// 	b: 2
+// };
+// let secondValue = {
+// 	b: 2,
+// 	a: 1
+// };
+
+// let firstValue = [1, 2, 3];
+// let secondValue = [1, 2, 3];
+
+let firstValue = 's';
+let secondValue = 'a';
+
+console.log(areTheyEqual(firstValue, secondValue));
--- 4. Funkcje w praktyce/getArraySum-medium/index.js
+++ 4. Funkcje w praktyce/getArraySum-medium/index.js
@@ -1,29 +1,56 @@
 import { validation } from "../../validation.js";
 
 let sum = 0;
+
 const getArraySum = (numbersArray, initialValue) => {
-    if (numbersArray.length > 0) {
-        sum += numbersArray[0] + numbersArray[1];
-        console.log(sum, numbersArray)
-        numbersArray.shift();
-        numbersArray.shift();
-        console.log(numbersArray)
-        getArraySum(numbersArray);
+
+    validation(
+        {
+            value: numbersArray,
+            type: 'Array'
+
+        },
+        {
+            value: initialValue,
+            type: 'Number'
+
+        })
+
+    if (numbersArray.length > 1) {
+
+        if (numbersArray.length > 0) {
+            sum += numbersArray[0] + numbersArray[1];
+            numbersArray.shift();
+            numbersArray.shift();
+            if (numbersArray.length != 0) getArraySum(numbersArray, 0);
+        }
+
+        if (numbersArray.length === 1) {
+            sum += numbersArray[0];
+            numbersArray.shift();
+        }
+
     }
 
-
-    return sum;
+    return sum + initialValue;
 }
 
-const exampleArray = [1, 2, 3, 4, 4, 5, 6, 6];
+const exampleArray = [1, 1, 5, 1, 1, 1, 1];
 const startingNumber = 5;
 console.log(getArraySum(exampleArray, startingNumber));
 
 
+
+// to explain (?)
 // const collatz = (n) => {
 //     if (n === 1) return 0;
 //     return n % 2 === 0 ? collatz(n / 2) + 1 : collatz(3 * n + 1) + 1;
 
 // };
 
-// console.log(collatz(21));
+// console.log(collatz(4));
+
+
+// jak to zrobić bez definiowania sum przed funkcją?
+
+
